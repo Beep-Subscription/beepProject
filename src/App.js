3df948f56ab18e4c1f0c4bca27bebc9864277c79
BitooBit.13.WEB!@ <web-bitoobit@yandex.ru>
2023-09-17 13:41:25 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Если вы это видите, значит вы не обкакались
         </p>
         <a
           className="App-link"
